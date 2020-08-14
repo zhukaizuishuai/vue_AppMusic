351bd7d04a1682c0700bda303823440edcb0a8c4
zhuk15 <zhuk15@chinaunicom.cn>
2020-08-14 11:08:02 +0800

    首次提交

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>singer</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
