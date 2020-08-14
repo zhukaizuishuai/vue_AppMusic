019f1e07e12eb715f212af0cbe1f4a7e8f701f47
zhuk15 <zhuk15@chinaunicom.cn>
2020-08-14 16:10:54 +0800

    组件提交

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/singer'
+      redirect: '/recommend'
     },
     {
       path: '/recommend',
